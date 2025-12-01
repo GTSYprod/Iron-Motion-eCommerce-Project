c8d08b339ff6caadccac0468b7982f62ab12bc04
masonrhodes <mrhodes@academic.rrc.ca>
2025-12-01 16:26:10 -0600

    Route config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # Admin routes
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  # User authentication (Requirement 3.1.4)
+  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
